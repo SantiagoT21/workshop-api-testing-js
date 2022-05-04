5fad9a2bce8b8ea92be8447f81ac35804100d9eb
Santiago Tamayo <stamayol@unal.edu.co>
2022-05-04 13:53:29 -0500

    Schema validation (testing). passed

--- test/MyFirstApiConsume.test.js
+++ test/MyFirstApiConsume.test.js
@@ -26,48 +26,59 @@ describe('First Api Tests', () => {
   });
 
   it('Consume HEAD Service', async () => {
-    const response = await agent.head('https://httpbin.org/get');
+    const response = await agent.head('https://httpbin.org/ip');
 
     expect(response.status).to.equal(statusCode.OK);
+    expect(response.headers).to.have.property('content-length');
+    expect(response.headers).to.have.property('content-type');
     expect(response.header['Content-Type'], /json/);
   });
 
-  it('Consume PATCH Service', async () => {
-    const json = {
+  it('Consume PATCH Service with query parameters', async () => {
+    const query = {
       name: 'John',
       age: '31',
       city: 'New York'
     };
 
-    const response = await agent.patch('https://httpbin.org/patch').send(json);
+    const response = await agent.patch('https://httpbin.org/patch').query(query);
 
     expect(response.status).to.equal(statusCode.OK);
-    expect(response.body.json).to.eql(json);
+    expect(response.body.data).to.eq('');
+    expect(response.body.args).to.eql(query);
   });
 
-  it('Consume PUT Service', async () => {
+  it('Consume PUT Service with sending Json', async () => {
     const json = {
       name: 'John',
       age: '31',
       city: 'New York'
     };
+    const header = {
+      Accept: '*/*'
+    };
 
-    const response = await agent.put('https://httpbin.org/put').send(json);
+    const response = await agent.put('https://httpbin.org/put').send(json).set(header);
 
     expect(response.status).to.equal(statusCode.OK);
     expect(response.body.json).to.eql(json);
+    expect(response.body.headers).to.have.property('Accept');
   });
 
-  it('Consume DELETE Service', async () => {
+  it('Consume DELETE Service with sending Json', async () => {
     const json = {
       name: 'John',
       age: '31',
       city: 'New York'
     };
+    const header = {
+      Accept: '*/*'
+    };
 
-    const response = await agent.del('https://httpbin.org/delete').send(json);
+    const response = await agent.del('https://httpbin.org/delete').send(json).set(header);
 
     expect(response.status).to.equal(statusCode.OK);
     expect(response.body.json).to.eql(json);
+    expect(response.body.headers).to.have.property('Accept');
   });
 });
